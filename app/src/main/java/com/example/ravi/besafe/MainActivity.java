350f0279c8402acc74f481c8d5d3f2860435ca81
Ravi <immaturegroup@gmail.com>
2018-06-06 23:07:48 +0530

    Menu Added!

--- app/src/main/java/com/example/ravi/besafe/MainActivity.java
+++ app/src/main/java/com/example/ravi/besafe/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.ravi.besafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
     }
+
 
     private boolean loadFragment(Fragment fragment){
         if(fragment!=null)
@@ -42,5 +46,28 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
 
         return loadFragment(fragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settingss,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.settings_start_service)
+        {
+            Toast.makeText(this, "Start Service", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(id==R.id.settings_change_number)
+        {
+            startActivity(new Intent(getApplicationContext(),ContactsSettings.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
