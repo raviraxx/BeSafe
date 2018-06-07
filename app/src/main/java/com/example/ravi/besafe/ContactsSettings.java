2a0e5e0bc78f93c6f8eff58c48855f552ddb5c6e
virenpunjabi <virenpunjabi97@gmail.com>
2018-06-07 13:32:30 +0530

    Location Feature and Contacts List Updated Commited By Viren Punjabi

--- app/src/main/java/com/example/ravi/besafe/ContactsSettings.java
+++ app/src/main/java/com/example/ravi/besafe/ContactsSettings.java
@@ -1,8 +1,12 @@
 package com.example.ravi.besafe;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,6 +34,7 @@ public class ContactsSettings extends AppCompatActivity {
     ArrayList<String> contactsList;
     ContactsListAdapter listAdapter;
     public static final int CONTACT_PICKER_REQUEST=500;
+    public static final int SEND_SMS_REQUEST=510;
     SharedPreferences contactsPref;
     SharedPreferences.Editor prefEditor;
 
@@ -69,50 +74,36 @@ public class ContactsSettings extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-//                String number=et_contact.getText().toString().trim();
-//
-//                if(TextUtils.isEmpty(number)){
-//                    et_contact.setError(getString(R.string.empty_number));
-//                    et_contact.requestFocus();
-//                    return;
-//
-//                    }
-//
-//                    if(number.length()!=10){
-//                        et_contact.setError(getString(R.string.invalid_number));
-//                        et_contact.requestFocus();
-//                        return;
-//                    }
-//
-//                    if(databaseHelper.readContacts().contains(number)){
-//                        Toast.makeText(ContactsSettings.this, "Contact Exists", Toast.LENGTH_SHORT).show();
-//                        return;
-//                    }
-//
-//                    if(databaseHelper.insertContact(number)){
-//                        //refreshTextView();
-//                        contactsList.add(number);
-//                        listAdapter=new ContactsListAdapter(ContactsSettings.this,contactsList);
-//                        lv_contacts.setAdapter(listAdapter);
-//                        et_contact.setText("");
-//                        Toast.makeText(ContactsSettings.this, ""+getString(R.string.insert_success), Toast.LENGTH_SHORT).show();
-//
-//                }
-//                    else{
-//
-//                        Toast.makeText(ContactsSettings.this, ""+getString(R.string.insert_failure), Toast.LENGTH_SHORT).show();
-//
-//                    }
-                new MultiContactPicker.Builder(ContactsSettings.this) //Activity/fragment context
-                        //.theme(R.style.MyCustomPickerTheme) //Optional - default: MultiContactPicker.Azure
-                        .hideScrollbar(false) //Optional - default: false
-                        .showTrack(true) //Optional - default: true
-                        .searchIconColor(Color.WHITE) //Option - default: White
-                        .setChoiceMode(MultiContactPicker.CHOICE_MODE_MULTIPLE) //Optional - default: CHOICE_MODE_MULTIPLE
-                        .handleColor(ContextCompat.getColor(ContactsSettings.this, R.color.colorPrimary)) //Optional - default: Azure Blue
-                        .bubbleColor(ContextCompat.getColor(ContactsSettings.this, R.color.colorPrimary)) //Optional - default: Azure Blue
-                        .bubbleTextColor(Color.WHITE) //Optional - default: White
-                        .showPickerForResult(CONTACT_PICKER_REQUEST);
+
+
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+
+                    new MultiContactPicker.Builder(ContactsSettings.this) //Activity/fragment context
+                            //.theme(R.style.MyCustomPickerTheme) //Optional - default: MultiContactPicker.Azure
+                            .hideScrollbar(false) //Optional - default: false
+                            .showTrack(true) //Optional - default: true
+                            .searchIconColor(Color.WHITE) //Option - default: White
+                            .setChoiceMode(MultiContactPicker.CHOICE_MODE_MULTIPLE) //Optional - default: CHOICE_MODE_MULTIPLE
+                            .handleColor(ContextCompat.getColor(ContactsSettings.this, R.color.colorPrimary)) //Optional - default: Azure Blue
+                            .bubbleColor(ContextCompat.getColor(ContactsSettings.this, R.color.colorPrimary)) //Optional - default: Azure Blue
+                            .bubbleTextColor(Color.WHITE) //Optional - default: White
+                            .showPickerForResult(CONTACT_PICKER_REQUEST);
+
+
+                }
+                else{
+
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                            requestPermissions(new String[]{Manifest.permission.SEND_SMS},SEND_SMS_REQUEST);
+                        }
+
+                    }
+
+
+                }
 
 
 
@@ -158,9 +149,18 @@ public class ContactsSettings extends AppCompatActivity {
     }
 
     public void gotoNext(View view) {
-        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-        finish();
+
+        if(databaseHelper.readContacts().size()>0){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }else{
+            Toast.makeText(this, "Insert atleast one contact", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -215,14 +215,5 @@ public class ContactsSettings extends AppCompatActivity {
 
     }
 
-    //    void refreshTextView(){
-//
-//        tv_contacts.setText("");
-//        ArrayList<String> contactsList= databaseHelper.readContacts();
-//
-//        for(String contact:contactsList){
-//            tv_contacts.append(contact+"\n\n");
-//        }
-//
-//    }
+
 }
