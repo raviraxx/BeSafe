0b6f5b4737b07f43185716686560094f9f85e43c
Ravi <immaturegroup@gmail.com>
2018-06-09 01:07:34 +0530

    Updates

---
+++ app/src/main/java/com/example/ravi/besafe/Constants.java
@@ -0,0 +1,13 @@
+package com.example.ravi.besafe;
+
+/**
+ * Created by ravi on 09-06-2018.
+ */
+
+public class Constants {
+    public static final String api_key="AIzaSyATb4Ol1J0RqJQoTEq-1UHG3_oMVc3y2Fk";
+
+    public static String getApi_key() {
+        return api_key;
+    }
+}
